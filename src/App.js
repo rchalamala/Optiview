9f496a2bb3f91f232c84e2f417ce694e5adec876
rchalamala <rchalamala@outlook.com>
2023-07-28 18:48:40 -0600

    formatting

--- src/App.js
+++ src/App.js
@@ -1,117 +1,142 @@
-import React, { useRef, useState } from 'react'
-import Geogebra from 'react-geogebra'
-import { evaluate } from './Components'
+import React, { useRef, useState } from "react";
+import Geogebra from "react-geogebra";
+import { evaluate } from "./Components";
 import { GeneticAlgorithm } from "./GeneticAlgorithm";
 import { ParticleSwarmOptimization } from "./ParticleSwarmOptimization";
 
 function App() {
-    const ref = useRef(null);
-    const [appletLoaded, setAppletLoaded] = useState(false);
-    const [functionEquation, setFunctionEquation] = useState("");
-    const [functionEquationLabel, setFunctionEquationLabel] = useState("");
-    const [functionValid, setFunctionValid] = useState(false);
-    const [dimension, setDimension] = useState(2);
-    const [algorithm, setAlgorithm] = useState("Genetic Algorithm");
+  const ref = useRef(null);
+  const [appletLoaded, setAppletLoaded] = useState(false);
+  const [functionEquation, setFunctionEquation] = useState("");
+  const [functionEquationLabel, setFunctionEquationLabel] = useState("");
+  const [functionValid, setFunctionValid] = useState(false);
+  const [dimension, setDimension] = useState(2);
+  const [algorithm, setAlgorithm] = useState("Genetic Algorithm");
 
-    const appletOnLoad = () => {
-        const app = window.mainDisplay;
+  const appletOnLoad = () => {
+    const app = window.mainDisplay;
 
-        app.setGridVisible(true);
+    app.setGridVisible(true);
 
-        setAppletLoaded(true)
+    setAppletLoaded(true);
 
-        // console.log("Applet Loaded");
+    // console.log("Applet Loaded");
+  };
+  const changeFunction = (event) => {
+    const app = window.mainDisplay;
+
+    let objectNames = app.getAllObjectNames();
+
+    for (let i = 0; i < objectNames.length; ++i) {
+      app.deleteObject(objectNames[i]);
     }
 
-    const changeFunction = (event) => {
-        const app = window.mainDisplay;
+    setFunctionEquation(event.target.value);
 
-        let objectNames = app.getAllObjectNames();
+    setFunctionEquationLabel(app.evalCommandGetLabels(event.target.value));
 
-        for(let i = 0; i < objectNames.length; ++i) {
-            app.deleteObject(objectNames[i]);
-        }
+    const selected = ref.current;
 
-        setFunctionEquation(event.target.value);
-
-        setFunctionEquationLabel(app.evalCommandGetLabels(event.target.value));
-
-        const selected = ref.current;
-
-        if(event.target.value.search("y") === -1) {
-            if(dimension !== 2) {
-                setDimension(2);
-                app.setPerspective('G');
-            }
-            setFunctionValid(evaluate(event.target.value, [0]) !== "" && !isNaN(evaluate(event.target.value, [0])));
-        } else {
-            if(dimension === 2) {
-                setDimension(3);
-                app.setPerspective('T');
-            }
-            setFunctionValid(evaluate(event.target.value, [0, 0]) !== "" && !isNaN(evaluate(event.target.value, [0, 0])));
-        }
-
-        selected.focus();
-
-        console.log("Function updated");
+    if (event.target.value.search("y") === -1) {
+      if (dimension !== 2) {
+        setDimension(2);
+        app.setPerspective("G");
+      }
+      setFunctionValid(
+        evaluate(event.target.value, [0]) !== "" &&
+          !isNaN(evaluate(event.target.value, [0])),
+      );
+    } else {
+      if (dimension === 2) {
+        setDimension(3);
+        app.setPerspective("T");
+      }
+      setFunctionValid(
+        evaluate(event.target.value, [0, 0]) !== "" &&
+          !isNaN(evaluate(event.target.value, [0, 0])),
+      );
     }
 
-    const changeAlgorithm = (event) => {
-        const app = window.mainDisplay;
+    selected.focus();
 
-        setAlgorithm(event.target.value);
+    console.log("Function updated");
+  };
 
-        let objectNames = app.getAllObjectNames();
+  const changeAlgorithm = (event) => {
+    const app = window.mainDisplay;
 
-        for(let i = 0; i < objectNames.length; ++i) {
-            if(objectNames[i] !== functionEquationLabel) {
-                app.deleteObject(objectNames[i]);
-            }
-        }
+    setAlgorithm(event.target.value);
+
+    let objectNames = app.getAllObjectNames();
+
+    for (let i = 0; i < objectNames.length; ++i) {
+      if (objectNames[i] !== functionEquationLabel) {
+        app.deleteObject(objectNames[i]);
+      }
     }
+  };
 
-    const renderAlgorithm = () => {
-        return algorithm === "Genetic Algorithm" ? <GeneticAlgorithm appletLoaded={appletLoaded} functionEquation={functionEquation} functionValid={functionValid} dimension={dimension}/> : <ParticleSwarmOptimization appletLoaded={appletLoaded} functionEquation={functionEquation} functionValid={functionValid} dimension={dimension}/>;
-    }
-
-    return (
-        <div className="min-h-screen flex flex-col md:flex-row items-center justify-center">
-            <div className="p-10">
-                <Geogebra
-                    id="mainDisplay"
-                    width="500"
-                    height="500"
-                    showMenuBar={false}
-                    showToolBar={false}
-                    appletOnLoad={appletOnLoad}
-                    errorDialogsActive={false}
-                    perspective={'G'}
-                />
-            </div>
-
-            <div className="ml-10">
-                <div className="py-2 w-half flex flex-col">
-                    <div className={functionValid ? "text-green-500" : "text-red-500"}>
-                        Equation (in terms of x and y):
-                    </div>
-                    <input className="border rounded px-2" type="text" value={functionEquation}
-                            onChange={changeFunction} ref={ref} disabled={!appletLoaded}/>
-                </div>
-
-                <div className="pb-2">
-                    Note: Right Click for Zoom to Fit
-                </div>
-
-                <select value={algorithm} onChange={changeAlgorithm}>
-                    <option value="Genetic Algorithm">Genetic Algorithm</option>
-                    <option value="Particle Swarm Optimization">Particle Swarm Optimization</option>
-                </select>
-
-                {renderAlgorithm()}
-            </div>
-        </div>
+  const renderAlgorithm = () => {
+    return algorithm === "Genetic Algorithm" ? (
+      <GeneticAlgorithm
+        appletLoaded={appletLoaded}
+        functionEquation={functionEquation}
+        functionValid={functionValid}
+        dimension={dimension}
+      />
+    ) : (
+      <ParticleSwarmOptimization
+        appletLoaded={appletLoaded}
+        functionEquation={functionEquation}
+        functionValid={functionValid}
+        dimension={dimension}
+      />
     );
+  };
+
+  return (
+    <div className="min-h-screen flex flex-col md:flex-row items-center justify-center">
+      <div className="p-10">
+        <Geogebra
+          id="mainDisplay"
+          width="500"
+          height="500"
+          showMenuBar={false}
+          showToolBar={false}
+          appletOnLoad={appletOnLoad}
+          errorDialogsActive={false}
+          perspective={"G"}
+        />
+      </div>
+
+      <div className="ml-10">
+        <div className="py-2 w-half flex flex-col">
+          <div className={functionValid ? "text-green-500" : "text-red-500"}>
+            Equation (in terms of x and y):
+          </div>
+          <input
+            className="border rounded px-2"
+            type="text"
+            value={functionEquation}
+            onChange={changeFunction}
+            ref={ref}
+            disabled={!appletLoaded}
+          />
+        </div>
+
+        <div className="pb-2">Note: Right Click for Zoom to Fit</div>
+
+        <select value={algorithm} onChange={changeAlgorithm}>
+          <option value="Genetic Algorithm">Genetic Algorithm</option>
+          <option value="Particle Swarm Optimization">
+            Particle Swarm Optimization
+          </option>
+        </select>
+
+        {renderAlgorithm()}
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/Components.js
+++ src/Components.js
@@ -1,80 +1,90 @@
 function evaluate(functionEquation, x) {
-    const app = window.mainDisplay;
+  const app = window.mainDisplay;
 
-    //console.log("Function value evaluated");
+  //console.log("Function value evaluated");
 
-    if(functionEquation.search('x') === -1) {
-        return "";
-    }
+  if (functionEquation.search("x") === -1) {
+    return "";
+  }
 
-    let x_substituted = functionEquation.replace(/x/g, "(" + x[0] + ")");
-    
-    if(x.length === 2) {
-        x_substituted = x_substituted.replace(/y/g, "(" + x[1] + ")");
-    }
+  let x_substituted = functionEquation.replace(/x/g, "(" + x[0] + ")");
 
-    let result = app.evalCommandCAS("Numeric(" + x_substituted + ", 50)");
+  if (x.length === 2) {
+    x_substituted = x_substituted.replace(/y/g, "(" + x[1] + ")");
+  }
 
-    if (result === '?' || result === "") {
-        return "";
-    } else {
-        return parseFloat(result);
-    }
+  let result = app.evalCommandCAS("Numeric(" + x_substituted + ", 50)");
+
+  if (result === "?" || result === "") {
+    return "";
+  } else {
+    return parseFloat(result);
+  }
 }
 
 function randomNumberInRange(minimum, maximum) {
-    // console.log("Random number generated");
+  // console.log("Random number generated");
 
-    return minimum + Math.random() * (maximum - minimum);
+  return minimum + Math.random() * (maximum - minimum);
 }
 
 // https://stackoverflow.com/a/35599181
 function gaussianRandom(mean, stdev) {
-    var y2;
-    var use_last = false;
-    var y1;
-    if (use_last) {
-        y1 = y2;
-        use_last = false;
-    } else {
-        var x1, x2, w;
-        do {
-            x1 = 2.0 * Math.random() - 1.0;
-            x2 = 2.0 * Math.random() - 1.0;
-            w = x1 * x1 + x2 * x2;
-        } while (w >= 1.0);
-        w = Math.sqrt((-2.0 * Math.log(w)) / w);
-        y1 = x1 * w;
-        y2 = x2 * w;
-        use_last = true;
-    }
+  var y2;
+  var use_last = false;
+  var y1;
+  if (use_last) {
+    y1 = y2;
+    use_last = false;
+  } else {
+    var x1, x2, w;
+    do {
+      x1 = 2.0 * Math.random() - 1.0;
+      x2 = 2.0 * Math.random() - 1.0;
+      w = x1 * x1 + x2 * x2;
+    } while (w >= 1.0);
+    w = Math.sqrt((-2.0 * Math.log(w)) / w);
+    y1 = x1 * w;
+    y2 = x2 * w;
+    use_last = true;
+  }
 
-    var retval = mean + stdev * y1;
-    if (retval > 0)
-        return retval;
-    return -retval;
+  var retval = mean + stdev * y1;
+  if (retval > 0) return retval;
+  return -retval;
 }
 
-
 function addPoints(functionEquation, values) {
-    const app = window.mainDisplay;
+  const app = window.mainDisplay;
 
-    let labels = [];
+  let labels = [];
 
-    for (let i = 0; i < values.length; ++i) {
-        const label = app.evalCommandGetLabels("(" + values[i].join() + "," + evaluate(functionEquation, values[i].slice(0, values[i].length)) + ")");
-        labels.push(label);
-    }
+  for (let i = 0; i < values.length; ++i) {
+    const label = app.evalCommandGetLabels(
+      "(" +
+        values[i].join() +
+        "," +
+        evaluate(functionEquation, values[i].slice(0, values[i].length)) +
+        ")",
+    );
+    labels.push(label);
+  }
 
-    return labels;
+  return labels;
 }
 
 function removePoints(labels) {
-    const app = window.mainDisplay;
+  const app = window.mainDisplay;
 
-    for (let i = 0; i < labels.length; ++i) {
-        app.deleteObject(labels[i])
-    }
+  for (let i = 0; i < labels.length; ++i) {
+    app.deleteObject(labels[i]);
+  }
 }
 
-export { evaluate, randomNumberInRange, gaussianRandom, addPoints, removePoints };
+export {
+  evaluate,
+  randomNumberInRange,
+  gaussianRandom,
+  addPoints,
+  removePoints,
+};
